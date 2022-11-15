83051be82b532a22991d49f7c04fc09318b8b36d
Shivam Veerabudren <shivamveerabudren@gmail.com>
2022-11-15 11:19:55 -0500

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Card from "./components/Card";
 
 function App() {
+
+  const apiKey = 'b4fe8bb3615246fa8da34cdea4aa44c3';
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`)
+    .then((response) => {setNews(response.data.articles); console.log(response.data.articles)});
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App grid grid-cols-3 gap-5">
+     {news.length === 0 ? "Loading" : 
+     news.map((obj) => <Card obj={obj} key={obj.url}/>)}
     </div>
   );
 }
 
 export default App;
+/**
+ * <Card obj={news[1]}/>
+ */
